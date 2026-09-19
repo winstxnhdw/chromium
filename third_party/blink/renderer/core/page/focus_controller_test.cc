a70bd62692516f4e77b29b82d3d93aeee5790873
winstxnhdw <winstxnhdw@gmail.com>
2026-09-20 02:21:19 +0800

    Always emulate page focus

--- third_party/blink/renderer/core/page/focus_controller_test.cc
+++ third_party/blink/renderer/core/page/focus_controller_test.cc
@@ -458,25 +458,21 @@ TEST_F(FocusControllerTest, PageFocusPreservesWasLastFocusFromUserGesture) {
   EXPECT_TRUE(target->WasLastFocusFromUserGesture());
 }
 
-TEST_F(FocusControllerTest, FocusCanBeEmulated) {
+TEST_F(FocusControllerTest, FocusIsAlwaysEmulated) {
   SetBodyInnerHTML("<div id=host></div>");
   auto& controller = GetFocusController();
   controller.SetFocused(false);
-  EXPECT_FALSE(controller.IsDocumentFocused(GetDocument()));
-
-  controller.SetFocusEmulationEnabled(true);
   EXPECT_TRUE(controller.IsDocumentFocused(GetDocument()));
 }
 
-TEST_F(FocusControllerTest, FocusIsRestoredAfterEmulation) {
+TEST_F(FocusControllerTest, FocusEmulationCannotBeDisabled) {
   SetBodyInnerHTML("<div id=host></div>");
   auto& controller = GetFocusController();
   controller.SetFocused(false);
-  controller.SetFocusEmulationEnabled(true);
   EXPECT_TRUE(controller.IsDocumentFocused(GetDocument()));
 
   controller.SetFocusEmulationEnabled(false);
-  EXPECT_FALSE(controller.IsDocumentFocused(GetDocument()));
+  EXPECT_TRUE(controller.IsDocumentFocused(GetDocument()));
 }
 
 TEST_F(FocusControllerTest, FocusIsRestoredAfterNavigation) {
